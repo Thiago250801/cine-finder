ecb392a82604f4bfe9b8accee6d1526f5062944b
Thiago Arthur <thlimabsgim@gmail.com>
2025-04-04 21:37:22 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './components/Header'
+
+
+function App() {
+
+  return (
+    <div className="bg-gray-800 min-h-screen">
+      <div>
+      <Header />
+      </div>
+      <h1 className='text-3xl'>Hello React</h1>
+    </div>
+  )
+}
+
+export default App
